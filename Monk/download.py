4e2697d1282783df04f89b69e16c5486e9fcbef6
Avraham Kahan <avrahamkahan123@gmail.com>
2024-08-18 11:34:18 +0300

    changed Monk

--- Monk/download.py
+++ Monk/download.py
@@ -1,10 +1,30 @@
 import os, shutil, subprocess, time, sys, requests, psutil, logging
 from uuid import getnode as get_mac
 
+
 def wait_on_subprocess(p: subprocess.Popen) -> bool:
     while p.poll() is None:
         time.sleep(2)
     return p.poll()==0
+
+
+def download_file(sample_id: str) -> bool:
+    gdc_token_fp = "/home/avraham/Abbot/Monk/token_01082024.txt"
+    gdc_client_path = "/home/avraham/gdc-client"
+    dest_path = f"/home/avraham/gdc_downloads/{sample_id}"  # fill in
+    download_command = [f"{gdc_client_path}", "download", f"{sample_id}", "-t", f"{gdc_token_fp}", "-d",
+                        "/home/avraham/gdc_downloads"]
+    download_subprocess = subprocess.Popen(download_command)
+    download_succeeded = wait_on_subprocess(download_subprocess)
+    return download_succeeded
+
+
+def add_phobos_file(is_female: bool, dest_path: str):
+    if is_female:
+        # softlink
+        os.system(f"ln -s /home/avraham/gdc_downloads/female_loci.phobos {dest_path}")
+    else:
+        os.system(f"ln -s /home/avraham/gdc_downloads/all_loci.phobos {dest_path}")
 
 
 def download_process():
@@ -33,21 +53,19 @@ def download_process():
         sample_id = req_json['sample_uuid']
         is_female = bool(req_json["is_female"])
         dest_path = f"/home/avraham/gdc_downloads/{sample_id}" # fill in
-        download_command = [f"{gdc_client_path}", "download", f"{sample_id}", "-t", f"{gdc_token_fp}", "-d", "/home/avraham/gdc_downloads"]
-        print(download_command)
-        download_subprocess = subprocess.Popen(download_command)
         logger.info(f"STARTED DOWNLOADING {sample_id}")
-        download_succeeded = wait_on_subprocess(download_subprocess)
+        download_succeeded = download_file(sample_id)
         if download_succeeded:
-            if is_female:
-                # softlink
-                os.system(f"ln -s /home/avraham/gdc_downloads/female_loci.phobos {dest_path}")
-            else:
-                os.system(f"ln -s /home/avraham/gdc_downloads/all_loci.phobos {dest_path}")
+            add_phobos_file(is_female, dest_path)
             logger.info(f"DOWNLOAD {sample_id} succesfully")
         else:
-            logger.error(f"FAILED DOWNLOAD {sample_id}")
-            exit()
+            logger.error(f"FAILED DOWNLOAD {sample_id}. TRYING AGAIN")
+            download_succeeded = download_file(sample_id)
+            if download_succeeded:
+                add_phobos_file(is_female, dest_path)
+            else:
+                logger.error(f"FAILED DOWNLOAD {sample_id} FOR SECOND TIME. EXITING")
+                exit()
         # handle failure
 
 
